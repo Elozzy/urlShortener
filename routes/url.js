d70b5369a1c9fa7ac8e11e522bd6b1018148900e
Elozzy <st.eloghosa@gmail.com>
2019-08-04 21:41:06 +0100

    works fine

--- routes/url.js
+++ routes/url.js
@@ -1,62 +1,53 @@
 const express = require('express');
 const router = express.Router();
-
 const validUrl = require('valid-url');
 const shortid = require('shortid');
-const config =require('config');
-
+const config = require('config');
 
 const Url = require('../models/Url');
 
-
-// @route POST /api/url/shorten
-//  Create short url
-
-
+// @route     POST /api/url/shorten
+// @desc      Create short URL
 router.post('/shorten', async (req, res) => {
-    const { longUrl } = req.body;
-    const baseUrl = config.get('baseUrl');
+  const { longUrl } = req.body;
+  const baseUrl = config.get('baseUrl');
 
-    //condition to check url validity
-    if(!validUrl.isUri(baseUrl)) {
-        return res.status(401).json('Invalid base url');
+  // Check base url
+  if (!validUrl.isUri(baseUrl)) {
+    return res.status(401).json('Invalid base url');
+  }
+
+  // Create url code
+  const urlCode = shortid.generate();
+
+  // Check long url
+  if (validUrl.isUri(longUrl)) {
+    try {
+      let url = await Url.findOne({ longUrl });
+
+      if (url) {
+        res.json(url);
+      } else {
+        const shortUrl = baseUrl + '/' + urlCode;
+
+        url = new Url({
+          longUrl,
+          shortUrl,
+          urlCode,
+          date: new Date()
+        });
+
+        await url.save();
+
+        res.json(url);
+      }
+    } catch (err) {
+      console.error(err);
+      res.status(500).json('Server error');
     }
-
-
-    // create url code
-
-    const urlCode = shortid.generate();
-
-    //check long url
-
-    if(validUrl.isUri(longUrl)){
-        try{
-            let url = await Url.findOne({ longUrl });
-            if(url) {
-                res.json(url);
-            } else {
-                const shortUrl = baseUrl + '/' + urlCode;
-
-                url = new Url({
-                    longUrl,
-                    shortUrl,
-                    urlCode,
-                    date: new Date()
-                });
-
-                await url.save();
-
-                res.json(url);
-            }
-        }catch (err){
-            console.log(err);
-            res.status(500).json('server error')
-        }
-    }else{
-        res.status(401).json('Invalid long url');
-
-    }
+  } else {
+    res.status(401).json('Invalid long url');
+  }
 });
-
 
 module.exports = router;
